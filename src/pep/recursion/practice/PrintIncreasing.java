bfb11f101e7fa6a75297aac21bd07b6c8587e493
Neeraj Kumbhkar <neerajkumbhkar2@gmail.com>
2022-02-03 02:31:53 +0530

    Practic Recursion

---
+++ src/pep/recursion/practice/PrintIncreasing.java
@@ -0,0 +1,18 @@
+package pep.recursion.practice;
+
+import java.util.Scanner;
+
+public class PrintIncreasing {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n =  sc.nextInt();
+            printIncreasingRec(n);
+        }
+    private static void printIncreasingRec(int n) {
+        if (n==0) return;
+        //excpaction pi(5)= 1 2 3 4 5
+        //faith (pi 4)print   1 2 3 4
+        printIncreasingRec(n-1);
+        System.out.println(n);
+    }
+}
