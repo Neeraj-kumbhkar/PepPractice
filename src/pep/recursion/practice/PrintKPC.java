67055546daebf62c5e2527bcdc8032af8f052bbb
Neeraj Kumbhkar <neerajkumbhkar2@gmail.com>
2022-02-06 03:55:58 +0530

    Practic Recursion

---
+++ src/pep/recursion/practice/PrintKPC.java
@@ -0,0 +1,27 @@
+package pep.recursion.practice;
+
+import java.util.Scanner;
+
+public class PrintKPC {
+    public static String[] mapArr = {".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        printKPC(str, "");
+    }
+
+    public static void printKPC(String str, String fsf) {
+        if(str.length()==0){
+            System.out.println(fsf);
+            return;
+        }
+        int digit = (int)str.charAt(0)-'0';
+        String keyPad = mapArr[digit];
+        for (char ch:keyPad.toCharArray()){
+            printKPC(str.substring(1), fsf+ch);
+        }
+
+    }
+
+}
+
