364389a3cf0c03e8556c406c80033192c206285e
Neeraj Kumbhkar <neerajkumbhkar2@gmail.com>
2022-02-08 04:43:53 +0530

    Practice Recursion an DP

--- src/dp/dynamic/programming/Fibnacci.java
+++ src/dp/dynamic/programming/Fibnacci.java
@@ -7,8 +7,20 @@ public class Fibnacci {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] qb = new int[n+1];
-        //System.out.println(fib(n, qb));
-        System.out.println(fibMemo(n,qb));
+        System.out.println("recursive "+fib(n, qb));
+        System.out.println("memoization "+fibMemo(n,qb));
+        System.out.println("tabulization "+fibTab(n,qb));
+    }
+
+    private static int fibTab(int N, int[] qb) {
+        for(int i = 0;i<N;i++){
+            if(i<=1){
+               qb[i]=i;
+               continue;
+            }
+            qb[i] = qb[i-1]+qb[i-2];
+        }
+        return qb[N];
     }
 
     private static int fib(int n, int[] qb) {
