5683dc29e106ca49f4a58953ecd0b313f3735885
Neeraj Kumbhkar <neerajkumbhkar2@gmail.com>
2022-02-27 01:37:21 +0530

    commit

--- src/tree/LevalOrderTravalsalWithMultiplApproaches.java
+++ src/tree/LevalOrderTravalsalWithMultiplApproaches.java
@@ -7,10 +7,15 @@ import java.util.ArrayList;
 import java.util.Queue;
 import java.util.Stack;
 
-public class LevalOrderInnextLine {
+public class LevalOrderTravalsalWithMultiplApproaches {
     private static class Node {
         int data;
         ArrayList< Node> children = new ArrayList< >();
+
+        private  Node() {}
+        private Node(int data) {
+            this.data=data;
+        }
     }
 
     public static void display(Node node) {
@@ -98,26 +103,87 @@ public class LevalOrderInnextLine {
     }
 
     public static void levelOrderLinewise(Node node) {
-        Queue< Node> queue = new ArrayDeque< >();
-        queue.add(node);
-
-        Queue< Node> cqueue = new ArrayDeque< >();
-        while (queue.size() > 0) {
-            node = queue.remove();
+        Queue<Node> mq = new ArrayDeque<>();
+        Queue<Node> cq = new ArrayDeque<>();
+        mq.add(node);
+        while (mq.size() > 0) {
+            ;
+            node = mq.remove();
             System.out.print(node.data + " ");
-
             for (Node child : node.children) {
-                cqueue.add(child);
+                cq.add(child);
             }
 
-            if (queue.size() == 0) {
-                queue = cqueue;
-                cqueue = new ArrayDeque< >();
+            if (mq.size() == 0) {
+                mq = cq;
+                cq = new ArrayDeque<>();
                 System.out.println();
             }
         }
     }
 
+    public static void levelOrderLinewise1(Node node) {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(node);
+        queue.add(new Node(-1));
+
+        while (queue.size() > 0) {
+            node = queue.remove();
+            if(node.data!=-1){
+                System.out.print(node.data + " ");
+                for (Node child : node.children) {
+                    queue.add(child);
+                }
+            } else{
+                if (queue.size() > 0) {
+                    queue.add(new Node(-1));
+                    System.out.println();
+                }
+            }
+
+        }
+    }
+
+
+    public static void levelOrderLinewise2(Node node) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(node);
+        while(q.size()>0){
+            int cicl = q.size();
+            for(int i = 0;i<cicl;i++){
+                node = q.remove();
+                System.out.print(node.data+" ");
+                for(Node child:node.children){
+                    q.add(child);
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static class Pair{
+        Node node;
+        int level;
+        public Pair(Node node,int level){
+            this.level=level;
+            this.node = node;
+        }
+    }
+    public static void levelOrderLinewisePair(Node node) {
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(node,1));
+        int level = 1;
+        while(q.size()>0){
+            Pair p = q.remove();
+            if(p.level>level){
+                level = p.level;
+                System.out.println();
+            }
+            System.out.print(p.node.data+" ");
+            for(Node child:p.node.children){
+                q.add(new Pair(child,level+1));
+            }
+        }
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -128,7 +194,10 @@ public class LevalOrderInnextLine {
         }
 
         Node root = construct(arr);
-        levelOrderLinewise(root);
+        //levelOrderLinewise(root);
+        //levelOrderLinewise1(root);
+        //levelOrderLinewise2(root);
+        levelOrderLinewisePair(root);
+
     }
 }
-
