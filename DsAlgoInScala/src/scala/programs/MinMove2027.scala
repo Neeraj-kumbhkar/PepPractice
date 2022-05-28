ea4310e2f72c2ee5ffa81c7502e20c8f2a8974c5
Neeraj Kumbhkar <neerajkumbhkar2@gmail.com>
2022-05-28 07:08:49 +0530

    commit

---
+++ DsAlgoInScala/src/scala/programs/MinMove2027.scala
@@ -0,0 +1,28 @@
+package scala.programs
+
+object MinMove2027 {
+  def minimumMoves(s: String): Int = {
+    var move = 0
+    var i=0
+    while(i<s.length()){
+      if(s.charAt(i)=='X'){
+        i=i+3
+        move=move+1
+      } else{
+        i=i+1
+      }
+    }
+    return move
+  }
+
+  def minimumMoves1(s: String): Int = {
+    val it = (s + "OO").toIterator
+    it.foldLeft(0)((acc, cur)=>{
+      if(cur == 'X'){it.next;it.next;acc + 1}
+      else acc
+    })
+  }
+  def main(args: Array[String]): Unit = {
+    println(minimumMoves("XXOXXX0XXXXX0X0X0X0XXX0X000X0X00XX000X0X0X00X00X00X0"))
+  }
+}
---
+++ DsAlgoInScala/src/scala/programs/RingAndRod2103.scala
@@ -0,0 +1,44 @@
+package scala.programs
+
+import scala.collection.immutable.HashSet
+import scala.collection.immutable.Nil.distinct
+
+object RingAndRod2103 {
+  def countPoints(rings: String): Int = {
+    val n = rings.length()-1
+    var arr = new Array[HashSet[Character]](10)
+
+    for (i<-0 to 9){
+      arr(i) = new HashSet[Character]()
+    }
+
+    for(i<-0 to n by 2){
+      val pair = rings.substring(i,i+2)
+      val ring = pair.charAt(0)
+      println(ring)
+      val rod = pair.charAt(1)-'0'
+      println(rod)
+      arr(rod)+=ring
+    }
+    var count=0
+    for(i<-0 to 9){
+      if(arr(i).size==3){
+        count=count+1
+      }
+    }
+    return count
+  }
+  def countPointsFunctional(rings: String): Int = {
+    val rods = rings.partition(char => char.isDigit)
+    rods._1.zip(rods._2)
+      .groupBy(element => element._1)
+      .map(seq => seq._2.map(ch => ch._2).toSet)
+      .count(seq => seq.size == 3)
+  }
+
+  def main(args: Array[String]): Unit = {
+      val s = "B0B6G0R6R0R6G9"
+      //println(countPoints(s))
+    println(countPointsFunctional(s))
+  }
+}
