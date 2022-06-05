0e42d5cb13072a3493709be7b7b49fede4031cd6
Neeraj Kumbhkar <neerajkumbhkar2@gmail.com>
2022-06-05 05:45:27 +0530

    commit

--- DsAlgoInScala/src/scala/programs/MinMove2027.scala
+++ DsAlgoInScala/src/scala/programs/MinMove2027.scala
@@ -12,17 +12,18 @@ object MinMove2027 {
         i=i+1
       }
     }
-    return move
+    move
   }
 
   def minimumMoves1(s: String): Int = {
-    val it = (s + "OO").toIterator
+    val it = (s+"00").iterator
     it.foldLeft(0)((acc, cur)=>{
-      if(cur == 'X'){it.next;it.next;acc + 1}
+      if(cur == 'X'){it.next();it.next();acc + 1}
       else acc
     })
   }
   def main(args: Array[String]): Unit = {
-    println(minimumMoves("XXOXXX0XXXXX0X0X0X0XXX0X000X0X00XX000X0X0X00X00X00X0"))
+    println(minimumMoves1("XXOXXX0XXXXX0X0X0X0XXX0X000X0X00XX000X0X0X00X00X00X0"))
+    //println(minimumMoves1("00000000"))
   }
 }
